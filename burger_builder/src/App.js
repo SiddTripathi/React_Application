905097beb1cd5eb7d3cfded195a148764b5a8ac7
Siddharth <siddhart.tripathi@gmail.com>
2020-08-04 23:30:23 +1200

    Css Module Test

--- burger_builder/src/App.js
+++ burger_builder/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
-
+import Layout from './Components/Layout/Layout'
+import rootclass from './index.module.css'
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <Layout className={rootclass.body}>
+          <p>Testing 1 2 3</p>
+        </Layout>
       </div>
     )
   }
