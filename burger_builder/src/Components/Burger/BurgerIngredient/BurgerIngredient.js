25adc784d4b0ff606a1ec63d7687a682ca4a803e
Siddharth <siddhart.tripathi@gmail.com>
2020-08-12 20:07:56 +1200

    Funcionality for adding Ingredients in Burger

--- burger_builder/src/Components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger_builder/src/Components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
 import classes from './BurgerIngredient.module.css'
-import BurgerBuilder from '../../../Container/BurgerBuilder/BurgerBuilder';
+//import BurgerBuilder from '../../../Container/BurgerBuilder/BurgerBuilder';
 
 
 class BurgerIngredient extends Component {
