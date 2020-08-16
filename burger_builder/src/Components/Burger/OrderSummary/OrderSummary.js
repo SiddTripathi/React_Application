d2f660c0cc67106199f4ee2fd8b5dee287eb3236
Siddharth <siddhart.tripathi@gmail.com>
2020-08-17 11:32:42 +1200

    Order Summary, Modal Backdrop, Cancel Continue buttons

--- burger_builder/src/Components/Burger/OrderSummary/OrderSummary.js
+++ burger_builder/src/Components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Auxiliary from '../../../hoc/Auxiliary'
+import Button from '../../UI/Button/Button'
 
 
 const orderSummary = (props) => {
@@ -18,6 +19,8 @@ const orderSummary = (props) => {
                 {ingredientSummary}
             </ul>
             <p>Continue to Checkout</p>
+            <Button btnType="Danger">CANCEL</Button>
+            <Button btnType="Success">CONTINUE</Button>
         </Auxiliary>
     )
 
